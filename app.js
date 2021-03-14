c5851d14d544b1e29c9e290f0ee03f0e3ccc5b10
Mislav <mislavjc@gmail.com>
2021-03-14 16:30:16 +0100

    initialized app using express and node

---
+++ app.js
@@ -0,0 +1,122 @@
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config();
+}
+
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const ejsMate = require("ejs-mate");
+const session = require("express-session");
+const flash = require("connect-flash");
+const methodOverride = require("method-override");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user");
+const ExpressError = require("./utils/ExpressError");
+
+const userRoutes = require("./routes/users");
+const MongoDBStore = require("connect-mongo")(session);
+
+const dbUrl = "mongodb://localhost:27017/fin"
+
+mongoose.connect(dbUrl, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+const app = express();
+
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
+
+// !Session
+
+const secret = process.env.SECRET || "thisshouldbeabettersecret!"
+
+const store = new MongoDBStore({
+    url: "mongodb://localhost:27017/fin",
+    secret,
+    touchAfter: 24 * 60 * 60,
+});
+
+store.on("error", function(e) {
+    console.log("SESSION STORE ERROR", e)
+})
+
+const sessionConfig = {
+    store,
+    name: "session",
+    secret,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+    },
+};
+
+app.use(session(sessionConfig));
+app.use(flash());
+
+// !Passport
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// !Flash, locals + User
+
+app.use((req, res, next) => {
+    if (!["/login", "register"].includes(req.originalUrl)) {
+        req.session.returnTo = req.originalUrl;
+    }
+    res.locals.currentUser = req.user;
+    res.locals.success = req.flash("success");
+    res.locals.error = req.flash("error");
+    next();
+});
+
+// !Routes
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+// !404
+
+app.all("*", (req, res, next) => {
+    // next(new ExpressError("Page Not Found", 404));
+    res.send("404")
+});
+
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = "Oh No, Something Went Wrong!";
+    res.status(statusCode).render("error", { err });
+});
+
+// !Start server
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Serving on port ${port}`);
+});
