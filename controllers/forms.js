74fd0ffb23d0934f1d52497389b840786a7692c3
Mislav <mislavjc@gmail.com>
2021-03-23 22:02:41 +0100

    added edit and update functionality

--- controllers/forms.js
+++ controllers/forms.js
@@ -2,6 +2,8 @@ const CreateForm = require("../models/createForm");
 const User = require("../models/user");
 const Form = require("../models/form");
 
+// !Form logic
+
 module.exports.storeForm = async (req, res) => {
     const form = new Form(req.body.form);
     form.owner = req.user._id;
@@ -26,9 +28,61 @@ module.exports.renderForm = async (req, res) => {
     res.render("forms/form", { formData, categoryNumber });
 };
 
+// !Main show page
+
 module.exports.formData = async (req, res) => {
     const user = req.user._id;
     const forms = await Form.find({ owner: user });
     const formData = await CreateForm.find({ owner: user });
     res.render("forms/show", { forms, formData });
+};
+// !More info page
+
+module.exports.showForm = async (req, res) => {
+    const user = req.user._id;
+    const formData = await CreateForm.find({ owner: user });
+    const form = await Form.findById(req.params.id);
+    res.render("forms/more", { form, formData });
+};
+
+// !Edit
+
+module.exports.renderEditForm = async (req, res) => {
+    const user = req.user._id;
+    const formData = await CreateForm.find({ owner: user });
+    const { id } = req.params;
+    const form = await Form.findById(id);
+    const kategorijaDD = [];
+    for (let i = 0; i < req.user.brojKategorija; i++) {
+        if (formData[0].kategorijaDropDown[i].includes(",")) {
+            kategorijaDD.push(formData[0].kategorijaDropDown[i].split(","));
+        } else {
+            kategorijaDD.push("");
+        }
+    }
+    formData[0].kategorijaDD = kategorijaDD;
+    const categoryNumber = req.user.brojKategorija;
+    if (!form) {
+        req.flash("error", "Ta narudžba ne postoji!");
+        return res.redirect("/show");
+    }
+    res.render("forms/edit", { form, formData, categoryNumber });
+};
+
+module.exports.updateForm = async (req, res) => {
+    const { id } = req.params;
+    const form = await Form.findByIdAndUpdate(id, { ...req.body.form });
+    await form.save();
+    console.log(form);
+    req.flash("success", "Uspješno promjenjena narudžba!");
+    res.redirect(`/show/${form._id}`);
+};
+
+// !Delete
+
+module.exports.deleteForm = async (req, res) => {
+    const { id } = req.params;
+    await Form.findByIdAndDelete(id);
+    req.flash("success", "Uspješno obrisana narudžba!");
+    res.redirect("/show");
 };
--- routes/forms.js
+++ routes/forms.js
@@ -7,4 +7,14 @@ router.route("/form").get(forms.renderForm).post(catchAsync(forms.storeForm));
 
 router.route("/show").get(catchAsync(forms.formData));
 
+router
+    .route("/show/:id")
+    .get(catchAsync(forms.showForm))
+    .delete(catchAsync(forms.deleteForm));
+
+router
+    .route("/show/:id/edit")
+    .get(catchAsync(forms.renderEditForm))
+    .put(catchAsync(forms.updateForm));
+
 module.exports = router;
