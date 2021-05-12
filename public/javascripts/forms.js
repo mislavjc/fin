dc8d9323d626f37d873374ee0350636351ee7c9d
Mislav <mislavjc@gmail.com>
2021-05-12 16:26:17 +0200

    final fixes

--- public/javascripts/forms.js
+++ public/javascripts/forms.js
@@ -1,25 +1,23 @@
-const range = document.getElementById("numOfCategories");
-const categoryValue = document.getElementById("categoryValue");
+const input = document.getElementById("numOfCategories");
 const valueArr = [1];
-range.addEventListener("input", function () {
-    let value = parseInt(range.value);
-    valueArr.push(value);
-    const list = document.getElementById(`num${value}`);
-    if (value != 1) {
-        list.classList.remove("d-none");
-    }
-    categoryValue.innerHTML = "Broj kategorija: " + value;
-    if (valueArr[valueArr.length - 2] > valueArr[valueArr.length - 1]) {
-        valueArr.splice(-2);
-        document
-            .getElementById(`num${valueArr.length + 1}`)
-            .classList.add("d-none");
-    }
+input.addEventListener("input", function () {
+  let value = parseInt(input.value);
+  valueArr.push(value);
+  const list = document.getElementById(`num${value}`);
+  if (value != 1) {
+    list.classList.remove("d-none");
+  }
+  if (valueArr[valueArr.length - 2] > valueArr[valueArr.length - 1]) {
+    valueArr.splice(-2);
+    document
+      .getElementById(`num${valueArr.length + 1}`)
+      .classList.add("d-none");
+  }
 });
 // !Progress bar
 const progressBar = document.querySelector(".progress-bar");
 for (let i = 0; i < 34; i++) {
-    setTimeout(function () {
-        progressBar.style = `width: ${i}%`;
-    }, i * 25);
+  setTimeout(function () {
+    progressBar.style = `width: ${i}%`;
+  }, i * 25);
 }
